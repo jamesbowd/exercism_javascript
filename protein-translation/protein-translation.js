091396824d0d7868fa288f467ad1421468e4d13d
James Bowden <jamesbowd@gmail.com>
2020-09-09 20:24:06 +0100

    protein-translation init

---
+++ protein-translation/protein-translation.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const translate = (rna) => {
+  var protein = [];
+
+  if (rna === undefined) {
+    return protein;
+  }
+};
