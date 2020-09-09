26455c2d3d7e9e2c1482766486e8cb0a2df3fd40
James Bowden <jamesbowd@gmail.com>
2020-09-09 21:01:43 +0100

    protein-translation all tests passed

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -3,10 +3,46 @@
 // convenience to get you started writing code faster.
 //
 
+const proteinTranslations = {
+  AUG: "Methionine",
+  UUU: "Phenylalanine",
+  UUC: "Phenylalanine",
+  UUA: "Leucine",
+  UUG: "Leucine",
+  UCU: "Serine",
+  UCC: "Serine",
+  UCA: "Serine",
+  UCG: "Serine",
+  UAU: "Tyrosine",
+  UAC: "Tyrosine",
+  UGU: "Cysteine",
+  UGC: "Cysteine",
+  UGG: "Tryptophan",
+  UAA: "STOP",
+  UAG: "STOP",
+  UGA: "STOP",
+};
+
 export const translate = (rna) => {
-  var protein = [];
+  var proteinArray = [];
 
   if (rna === undefined) {
-    return protein;
+    return proteinArray;
   }
+
+  for (let i = 0; i < rna.length; i += 3) {
+    let codon = rna.substring(i, i + 3);
+
+    if (proteinTranslations.hasOwnProperty(codon) === false) {
+      throw new Error("Invalid codon");
+    }
+
+    let protein = proteinTranslations[codon];
+    if (protein === "STOP") {
+      return proteinArray;
+    } else {
+      proteinArray.push(protein);
+    }
+  }
+  return proteinArray;
 };
