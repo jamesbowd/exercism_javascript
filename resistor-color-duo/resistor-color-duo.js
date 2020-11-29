4d73c670311f4b20562931a2104cc5a6b26839a5
James Bowden <jamesbowd@gmail.com>
2020-11-29 21:08:47 +0000

    resister-color-duo exercise complete

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,87 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+// The colors are mapped to the numbers from 0 to 9 in the sequence:
+// Black - Brown - Red - Orange - Yellow - Green - Blue - Violet - Grey - White
+
+// From the example above:
+// brown-green should return 15
+// brown-green-violet should return 15 too, ignoring the third color.
+
+export const decodedValue = (arr) => {
+  let value = "";
+  for (let i = 0; i < 2; i++) {
+    switch (arr[i]) {
+      case "black":
+        value += "0";
+        break;
+      case "brown":
+        value += "1";
+        break;
+      case "red":
+        value += "2";
+        break;
+      case "orange":
+        value += "3";
+        break;
+      case "yellow":
+        value += "4";
+        break;
+      case "green":
+        value += "5";
+        break;
+      case "blue":
+        value += "6";
+        break;
+      case "violet":
+        value += "7";
+        break;
+      case "grey":
+        value += "8";
+        break;
+      case "white":
+        value += "9";
+        break;
+    }
+  }
+  return parseInt(value);
 };
+
+// const decodedValue = (arr) => {
+//   let value = 0;
+//   for (color of arr) {
+//     switch (color) {
+//       case "black":
+//         value += 0;
+//         break;
+//       case "brown":
+//         value += 1;
+//         break;
+//       case "red":
+//         value += 2;
+//         break;
+//       case "orange":
+//         value += 3;
+//         break;
+//       case "yellow":
+//         value += 4;
+//         break;
+//       case "green":
+//         value += 5;
+//         break;
+//       case "blue":
+//         value +=6;
+//         break;
+//       case "violet":
+//         value +=7;
+//         break;
+//       case "grey":
+//         value += 8;
+//         break;
+//       case "white":
+//         value += 9;
+//         break;
+//     }
+//   }
+//   return value;
+// };
